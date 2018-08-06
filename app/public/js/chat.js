8a9d3d8fa5337b6d04e2af94f5c624c9fd749f7c
ubarum004 <a538642@hotmail.com>
2018-08-06 11:06:53 -0700

    done

--- app/public/js/chat.js
+++ app/public/js/chat.js
@@ -7,7 +7,7 @@ socket.on('connect', function(){
 		var chatUsername = document.querySelector('#chat-username');
 		var chatMessage = document.querySelector('#chat-message');
 		
-		chatForm.addEventListener('submit',function(e){
+		chatForm.one('submit',function(e){
 			e.preventDefault();
 			
 			socket.emit('postMessage',{
